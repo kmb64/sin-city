a9a1750af422339c6dc18b9ab9aee97e9c893e7d
kmb64 <km_balemi@hotmail.com>
2014-07-25 01:07:32 +1000

    Profiles now source from json data.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,35 @@
 angular.module('gemsApp', [])
-  .service('profileService', ['$http',function($http){
-    return function(){
-      var getProfiles = function(){
+  .service('profileService', ['$http', function ($http) {
+    return function () {
+      var getProfiles = function () {
 
         return $http({method: 'GET', url: '/profiles.json'}).
-          success(function(data, status, headers, config) {
+          success(function (data, status, headers, config) {
             return angular.fromJson(data);
           }).
-          error(function(data, status, headers, config) {
+          error(function (data, status, headers, config) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
           });
       };
       return {
-        getProfiles : getProfiles
+        getProfiles: getProfiles
       }
     }
 
   }])
-  .controller('profileCtrl', ['$scope','profileService', function ($scope, profileService) {
+  .controller('profileCtrl', ['$scope', 'profileService', function ($scope, profileService) {
 
-  $scope.profile = {};
-  $scope.profile.gemma = profileService().getProfiles();
-  console.log($scope.profile.gemma);
+    $scope.profile = {};
 
-}]);
+    var profilePromise = profileService().getProfiles();
+    profilePromise.then(function(response){
+      $scope.profile.girls = response.data.girls;
+    }, function(){
+      console.log('Failed to retrieve profiles.');
+    });
+
+//    $scope.profile.gemma =
+//      console.log($scope.profile.gemma);
+
+  }]);
